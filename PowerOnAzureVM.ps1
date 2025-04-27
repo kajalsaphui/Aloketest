2f11d9aed23fc61eb06c41e5979054827cdf6a13
kajalsaphui <70341664+kajalsaphui@users.noreply.github.com>
2025-04-27 20:12:33 +0530

    Update PowerOnAzureVM.ps1

--- PowerOnAzureVM.ps1
+++ PowerOnAzureVM.ps1
@@ -1,7 +1,9 @@
 Connect-AzAccount]
 #< Prompt for username and password
-$username = Read-Host -Prompt "Enter your Azure username"
-$password = Read-Host -Prompt "Enter your Azure password" -AsSecureString
+#$username = Read-Host -Prompt "Enter your Azure username"
+$username = Admin@ksaphuigmail322.onmicrosoft.com
+$password = ESPL@0987654321
+#$password = Read-Host -Prompt "Enter your Azure password" -AsSecureString
 #Get-AzVM
 #Start-AzVM -ResourceGroupName <ResourceGroupName> -Name <VMName>
 Get-AzVM | Where-Object {$_.PowerState -eq "VM deallocated"} | ForEach-Object {Start-AzVM -ResourceGroupName $_.ResourceGroupName -Name $_.Name}
